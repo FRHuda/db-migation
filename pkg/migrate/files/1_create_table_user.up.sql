2408130f6320a66d6596ed8ed0ac81843c990fae
Huda <furqonrhuda@gmail.com>
2021-07-29 00:21:21 +0700

    create example migration

---
+++ pkg/migrate/files/1_create_table_user.up.sql
@@ -0,0 +1,9 @@
+-- Table Definition ----------------------------------------------
+
+CREATE TABLE "user" (
+    id text,
+    name text,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone
+);
